a8fdb28c8def5882aa458948334ff5f5c8873b52
chicomito76 <chicomito76@gmail.com>
2026-08-26 20:46:45 -0400

    Add files via upload

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,5 +1,5 @@
-const CACHE='radar-futbol-v47-fondo';
-const CORE=['/','/index.html','/manifest.webmanifest','/icon.svg','/bg-radar-v47.png'];
+const CACHE='radar-futbol-v47a-portada-ui';
+const CORE=['/','/index.html','/manifest.webmanifest','/icon.svg','/bg-radar-v47a.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
